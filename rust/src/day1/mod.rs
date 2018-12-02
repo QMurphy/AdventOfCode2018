8bfb568f8edf997c6d289e2fc1ec5eb70b73ad06
Quinn Murphy <qmurphy64@gmail.com>
2018-12-02 17:33:38 -0600

    Add Day 1 Rust solution

---
+++ rust/src/day1/mod.rs
@@ -0,0 +1,52 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::collections::HashSet;
+
+static INPUT_FILE: &str = "../input/day1.txt";
+
+pub fn part1() -> i32
+{
+    let input_file = File::open( INPUT_FILE ).expect( "Unable to open day1.txt" );
+    let reader = BufReader::new( input_file );
+    
+    let mut freq : i32 = 0;
+    for line in reader.lines()
+    {
+        let delta = line.unwrap().parse::<i32>().unwrap();
+        freq = freq + delta;
+    }
+    
+    return freq;
+}
+
+pub fn part2() -> i32
+{
+    let mut all_freqs = HashSet::new();
+    let mut freq : i32 = 0;
+    all_freqs.insert( freq ); // Initial frequency
+    let mut dup_found = false;
+
+    while !dup_found
+    {
+        let input_file = File::open( INPUT_FILE ).expect( "Unable to open day1.txt" );
+        let reader = BufReader::new( input_file );
+
+        for line in reader.lines()
+        {
+            let delta = line.unwrap().parse::<i32>().unwrap();
+            freq = freq + delta;
+            if all_freqs.contains( &freq )
+            {
+                dup_found = true;
+                break;
+            }
+            else
+            {
+                all_freqs.insert( freq );
+            }
+        }
+    }
+    
+    return freq;
+}
---
+++ rust/src/main.rs
@@ -0,0 +1,7 @@
+mod day1;
+
+fn main()
+{
+    println!( "Part 1: {}", day1::part1() );
+    println!( "Part 2: {}", day1::part2() );
+}
